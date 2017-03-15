61a8581d80cc485f5839ea3f07c8d5813d9c40ba
Andres Teran <aftosb95>
2017-03-15 09:34:53 -0500

    Cambios llevados a cabo. 1- aagrego estilo a botones 2 se agrege la barra toolbar 3

--- app/src/main/java/com/example/andresteran/repasoparcial/CreateAccount.java
+++ app/src/main/java/com/example/andresteran/repasoparcial/CreateAccount.java
@@ -2,6 +2,7 @@ package com.example.andresteran.repasoparcial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.*;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -9,6 +10,15 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        showToolbar(getResources().getString(R.string.str_title_toolbar),true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
 
     }
 }
